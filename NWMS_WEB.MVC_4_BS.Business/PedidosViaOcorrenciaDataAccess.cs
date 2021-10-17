fbfa298ed836d56e3b4bc30fef3726e7ca7d9833
Josimesio.pereira <sistema02@nutriplan.com.br>
2021-10-17 18:05:28 -0300

    teste

--- NWMS_WEB.MVC_4_BS.Business/PedidosViaOcorrenciaDataAccess.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NUTRIPLAN_WEB.MVC_4_BS.Business
-{
-    internal class PedidosViaOcorrenciaDataAccess
-    {
-        public PedidosViaOcorrenciaDataAccess()
-        {
-        }
-    }
-}
